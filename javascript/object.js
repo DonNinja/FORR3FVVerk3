afe6825cb76cd1a488a11991ee5b2b7f8c8b0c0e
DonNinja <yngvileo@gmail.com>
2018-10-03 15:36:24 +0000

    er að byrja á drag event

--- javascript/object.js
+++ javascript/object.js
@@ -2,6 +2,8 @@
 
 var scene = new THREE.Scene(); //setur upp scene
 
+scene.background = new THREE.Color( 0x000000 );
+
 var camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 1, 10000 ); //setur upp camera
 			   //THREE.PerspectiveCamera( fov, aspect, near, far );
 
@@ -11,20 +13,19 @@ document.body.appendChild(renderer.domElement);
 
 var geometry = new THREE.CylinderGeometry( 1, 200, 700, 20, 20, false );
 
-var material = new THREE.MeshBasicMaterial({color: 0xfffff, wireframe: true});
+var material = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
 
 var pyralinder = new THREE.Mesh(geometry, material);
 scene.add(pyralinder);
 
 camera.position.z = 1000;
 
-
-
 function render() {
    	requestAnimationFrame(render);
-   	pyralinder.rotation.x += 0.01;
+   	/*pyralinder.rotation.x += 0.01;
 	pyralinder.rotation.y += 0.02;
-	pyralinder.rotation.z += 0.05;
+	pyralinder.rotation.z += 0.05;*/
 	renderer.render(scene, camera);
 }
 render();
+
